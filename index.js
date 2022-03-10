1dda59698cedff58054cdc2594ff5df2694641c9
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-10 16:43:04 +0500

    nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.send("Hello World")
 })
 
 app.listen(5001, () => {console.log("Server has been started on Port 5001...")})
