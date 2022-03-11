e3c39f5833955bc25f243cc3d5a8be2deb126467
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-11 23:42:42 +0500

    Improved post:id

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.get("/contact", (req, res) => {
     res.render("contact")
 })
 
-app.get("/post", (req, res) => {
-    res.render("post")
+app.get("/post/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render("post", {post})
 })
 
 app.get("/posts/new", (req, res) => {
