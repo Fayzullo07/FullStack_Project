2a41ba72fae1cbe58883d1e3d9e772b52c7cd6bc
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-18 23:12:04 +0500

    Register and npm i bcrypt

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const expressEdge = require("express-edge");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+
+
 const homePageController = require("./controllers/homePage");
 const getPostsController = require("./controllers/getPosts");
 const postsNewController = require("./controllers/postsNew");
 const createPostController = require("./controllers/createPost");
-const validatCreatePostMiddleware = require("./middleware/validationMiddleware")
+const createUserController = require('./controllers/createUser');
+const storeUserController = require("./controllers/userStore")
 
+const validatCreatePostMiddleware = require("./middleware/validationMiddleware");
 const app = express();
 
 mongoose.connect("mongodb+srv://fayzullo:F4995875f@cluster0.tpf56.mongodb.net/node_blog");
@@ -22,6 +26,8 @@ app.use(express.urlencoded({extended: true}));
 app.get("/", homePageController);
 app.get("/post/:id", getPostsController);
 app.get("/posts/new", postsNewController);
-app.post("/posts/create", validatCreatePostMiddleware, createPostController)
+app.post("/posts/create", validatCreatePostMiddleware, createPostController);
+app.get("/reg", createUserController);
+app.post("/auth/reg", storeUserController);
 
 app.listen(5000, () => {console.log("Server has been started on Port 5000...")})
