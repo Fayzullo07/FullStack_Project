d7e0a553dfbae79c82e8939887f3b0b2163b281e
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-18 21:51:44 +0500

    MVC and Middleware

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const path = require("path");
 const expressEdge = require("express-edge");
 const mongoose = require("mongoose");
-const Post = require("./models/Post");
 const fileUpload = require("express-fileupload");
+const homePageController = require("./controllers/homePage");
+const getPostsController = require("./controllers/getPosts");
+const postsNewController = require("./controllers/postsNew");
+const createPostController = require("./controllers/createPost");
+const validatCreatePostMiddleware = require("./middleware/validationMiddleware")
 
 const app = express();
 
@@ -16,38 +19,9 @@ app.set("views", `${__dirname}/views`)
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", async (req, res) => {
-    const posts = await Post.find();
-    res.render("index", {posts})
-})
-
-app.get("/about", (req, res) => {
-    res.render("about")
-})
-
-app.get("/contact", (req, res) => {
-    res.render("contact")
-})
-
-app.get("/post/:id", async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render("post", {post})
-})
-
-app.get("/posts/new", (req, res) => {
-    res.render("create")
-})
-
-app.post("/posts/create", (req, res) => {
-    const { image } = req.files;
-    image.mv(path.resolve(__dirname, "public/posts", image.name), (err) => {
-        if(err){
-            console.log(err)
-        }
-        Post.create({...req.body, image: `/posts/${image.name}`}, (err, post) => {
-            res.redirect('/')
-        })
-    })
-})
+app.get("/", homePageController);
+app.get("/post/:id", getPostsController);
+app.get("/posts/new", postsNewController);
+app.post("/posts/create", validatCreatePostMiddleware, createPostController)
 
 app.listen(5000, () => {console.log("Server has been started on Port 5000...")})
