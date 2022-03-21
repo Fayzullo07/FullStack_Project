042c4d061f5d24f68df6e4f1af9820be91b6c2d5
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-21 12:21:36 +0500

    summernote.org

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if(!(req.files && req.files.image) || !req.body.title || !req.body.username || !req.body.description || !req.body.content){
+    if(!(req.files && req.files.image) || !req.body.title || !req.body.description || !req.body.content){
         return res.redirect("/posts/new")
     }
     next();
